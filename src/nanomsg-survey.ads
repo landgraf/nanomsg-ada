0ae22818e79b500521a0af4aa0c40b7cf1d88038
Pavel Zhukov <landgraf@fedoraproject.org>
2015-11-28 16:04:02 +0100

    Fix survey protocol (Fix #1)

--- src/nanomsg-survey.ads
+++ src/nanomsg-survey.ads
@@ -22,7 +22,7 @@
 
 package Nanomsg.Survey is 
    Nn_Proto_Survey : constant            := 6;
-   Nn_Surveyor     : constant Protocol_T := Nn_Proto_Survey * 16 + 0;
-   Nn_respondent   : constant Protocol_T := Nn_Proto_Survey * 16 + 1;
+   Nn_Surveyor     : constant Protocol_T := Nn_Proto_Survey * 16 + 2;
+   Nn_respondent   : constant Protocol_T := Nn_Proto_Survey * 16 + 3;
    Nn_Surveyor_Deadline : constant := 1;
 end Nanomsg.Survey;
